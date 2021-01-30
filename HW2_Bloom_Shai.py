3a4014a8c178d8cb67f9b639e813467e44773fae
fourchecker4 <77808387+fourchecker4@users.noreply.github.com>
2021-01-30 14:43:05 -0500

    Add files via upload

---
+++ HW2_Bloom_Shai.py
@@ -0,0 +1,24 @@
+favcolor = ['blue', 'green', 'red', 'yellow', 'black']
+for color in favcolor:
+    print(color)
+    
+for n in range(10):
+    print(n)
+    
+n = 0
+while n < 10:
+    print(n)
+    n += 1
+
+n = 0
+while n <= 10:
+    print(n)
+    n += 1
+print ('greater than 10')
+
+i = 1
+total = 0
+while i <= 30:
+    total += i
+    i += 1
+print(total)
